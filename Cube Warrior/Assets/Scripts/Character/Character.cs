86a2f9f7f129fcb6aae38bbf9133aba198f12cae
Arne De Pontieu <arne@depontieu.net>
2022-02-01 20:27:28 +0200

    Test movement added

---
+++ Cube Warrior/Assets/Scripts/Character/Character.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+namespace Character
+{
+    public class Character : MonoBehaviour, IMoveable, IDamageable
+    {
+        [SerializeField] private Rigidbody characterController;
+
+        public float MovementSpeed { get; set; }
+
+        private void Awake()
+        {
+            MovementSpeed = 10f;
+        }
+
+        public void Move(Vector3 direction, float speed)
+        {
+            Vector3 movementVector = direction.normalized * speed;
+            characterController.velocity = movementVector;
+            Debug.Log($"Moving -> {movementVector}");
+        }
+
+        public void TakeDamage(float amount, DamageType damageType)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Update()
+        {
+            HandleInput();
+        }
+
+        private void HandleInput()
+        {
+            Vector3 movementVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            Move(movementVector * Time.deltaTime, MovementSpeed);
+        }
+    }
+}
---
+++ Cube Warrior/Assets/Scripts/Character/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character
+{
+    public class Enemy : MonoBehaviour, IMoveable
+    {
+        [SerializeField] private Character player;
+        [SerializeField] private Rigidbody rigidBody;
+
+        public float MovementSpeed { get; set; }
+
+        public void Move(Vector3 direction, float speed)
+        {
+            Vector3 movementVector = direction.normalized * speed;
+            rigidBody.velocity = movementVector;
+        }
+
+        private void Update()
+        {
+            Move(player.transform.position - transform.position, MovementSpeed);
+        }
+
+        private void Awake()
+        {
+            MovementSpeed = 3f;
+        }
+    }
+}
